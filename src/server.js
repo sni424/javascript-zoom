3cb87c0e939e9f8f8071f0d305990f7be1a35e41
sni424 <jojonghyeok424@gmail.com>
2022-09-19 00:27:36 +0900

    닉네임과 채팅 설정

--- src/server.js
+++ src/server.js
@@ -5,9 +5,12 @@ import { WebSocketServer } from "ws";
 
 const __dirname = path.resolve();
 const app = express();
-
+/**pug페이지들을  렌더하기위해 */
+/** pug로 view enggine */
 app.set("view engine", "pug");
+/**템플릿이 어디에있는지 지정 */
 app.set("views", __dirname + "/src/views");
+/**public url생성 */
 app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
@@ -15,6 +18,7 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
+/**http와 ws를 같이 실행 */
 const wss = new WebSocketServer({ server });
 
 function onSocketClose() {
@@ -25,10 +29,21 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✅");
     socket.on("close", onSocketClose);
-    socket.on("message", (message) => {
-        sockets.forEach((aSocket) => aSocket.send(message));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) =>
+                    aSocket.send(`${socket.nickname}: ${message.payload}`)
+                );
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
     });
 });
 
