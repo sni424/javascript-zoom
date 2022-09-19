510eefe6499833e3d0a25b30923bc40cdafede63
sni424 <jojonghyeok424@gmail.com>
2022-09-20 01:00:31 +0900

    socket.io 이벤트 argument3개

--- src/server.js
+++ src/server.js
@@ -24,7 +24,12 @@ const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
 wsServer.on("connection", (socket) => {
-    console.log(socket);
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done();
+        }, 10000);
+    });
 });
 // const sockets = [];
 
